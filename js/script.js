e073d1fcdc96121437fc3575d3f61268c8028987
mhmd2004 <memoshufoot99@gmail.com>
2020-04-13 11:22:43 +0300

    add

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+$('[data-add-to-cart]').click(function(e) {
+    alert('أضيف المُنتج إلى عربة الشراء');
+    e.stopPropagation();
+});
